93e880d606102852570e2659dea71c02055c2667
Stanley Chisom <stanley.chisom042@gmail.com>
2024-01-09 20:52:31 +0100

    routes and auth setup

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const RootPage = () => {
+  return (
+    <div>
+      <h4>Root Page (Protected)</h4>
+    </div>
+  );
+};
+
+export default RootPage;
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="text-gray-800 text-xl">Hello Hoomans!</div>;
-}
